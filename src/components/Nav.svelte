dcbc42c881a4f1abef67e251ac08de685a2c8ecb
Chris Boakes <chris@chrisboakes.com>
2020-05-02 16:43:11 +0100

    Navigation component

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,58 +3,59 @@
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
+	div {
+		display: flex;
+		flex-direction: column;
+		padding-top: 1rem;
+		text-align: center;
+	}
+
+	h1 {
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+		font-size: 1.2rem;
+		text-transform: uppercase;
 	}
 
 	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
+		border-bottom: 1px solid #dfdfdf;
+		border-top: 1px solid #dfdfdf;
+		display: flex;
+		justify-content: center;
+		padding-bottom: 1rem;
+		padding-top: 1rem;
 	}
 
 	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
+		margin-right: 1rem;
 	}
 
 	a {
 		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
+	}
+
+	.current {
+		text-decoration: underline;
 	}
 </style>
 
-<nav>
-	<ul>
-		<li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
-		<li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
+<div>
+	<header>
+		<svg width="50" height="36" xmlns="http://www.w3.org/2000/svg"><path d="M49.493 10.155a1.26 1.26 0 00-1.53.005l-13.16 10.045L26.164.69c-.41-.92-1.916-.92-2.326 0L15.1 20.433 2.054 10.172a1.269 1.269 0 00-1.968 1.457L9.15 35.044a1.268 1.268 0 001.183.81h29.335a1.263 1.263 0 001.184-.81l9.062-23.415a1.268 1.268 0 00-.421-1.474zM38.798 33.314H11.204L4.126 15.032l10.69 8.404c.308.244.707.327 1.092.238a1.278 1.278 0 00.856-.721l8.238-18.61 8.129 18.363a1.275 1.275 0 001.936.497l10.859-8.291-7.128 18.402z"/></svg>
+		<h1>Sapper Demo</h1>
+	</header>
 
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li>
-	</ul>
-</nav>
+	<nav>
+		<ul>
+			<li>
+				<a href="." class='{ segment === undefined ? 'current' : '' }'>
+					Listings
+				</a>
+			</li>
+			<li>
+				<a href="about" class='{ segment === 'about' ? 'current' : '' }'>
+					About
+				</a>
+			</li>
+		</ul>
+	</nav>
+</div>
