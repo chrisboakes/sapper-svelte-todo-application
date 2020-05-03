15d82a22ef2983b55f761b886578f26670bdab7c
Chris Boakes <chris@chrisboakes.com>
2020-05-03 13:40:12 +0100

    Filter the listings data

--- src/components/Listings.svelte
+++ src/components/Listings.svelte
@@ -3,16 +3,26 @@
     import Filters from './Filters.svelte';
 
     export let todos;
+    export let filteredContent = todos;
 
     function filterData(event) {
-        console.log(event.detail);
+        // Show all data
+        if (event.detail === 'all') {
+            filteredContent = todos;
+        // Only show completed items
+        } else if (event.detail === 'complete') {
+            filteredContent = todos.filter(item => item.completed);
+        // Only show incomplete items
+        } else {
+            filteredContent = todos.filter(item => !item.completed);
+        }
     }
 </script>
 
 <Filters on:updatefilter={ filterData } />
 
 <ul>
-    {#each todos as todo}
+    {#each filteredContent as todo}
         <Card data={ todo } />
     {/each}
 </ul>
