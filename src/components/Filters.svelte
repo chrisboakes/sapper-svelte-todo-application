a509ca7776dcb591f03cc8ce73280d6d26f8e0bb
Chris Boakes <chris@chrisboakes.com>
2020-05-03 13:13:57 +0100

    Basic filter

---
+++ src/components/Filters.svelte
@@ -0,0 +1,19 @@
+<style>
+    div {
+        padding-bottom: 1.5rem;
+        margin: 0 auto;
+        width: 330px;
+    }
+
+    select {
+        width: 100%;
+    }
+</style>
+
+<div>
+    <select>
+        <option value="all">All</option>
+        <option value="incomplete">Incomplete</option>
+        <option value="complete">Complete</option>
+    </select>
+</div>
--- src/components/Listings.svelte
+++ src/components/Listings.svelte
@@ -1,8 +1,11 @@
 <script>
     import Card from './Card.svelte';
+    import Filters from './Filters.svelte';
 
     export let todos;
 </script>
+
+<Filters />
 
 <ul>
     {#each todos as todo}
